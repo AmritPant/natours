1c4850926274e4578c08d82a9fb6ca904469c30e
Amrit Pant <info.amritpant@gmail.com>
2022-07-19 15:01:34 +0545

    Added , [top-5-cheap, stats, monthlyplan] routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,13 @@ const router = express.Router();
 // router.param('id', tourController.checkID);
 
 router
+    .route('/top-5-cheap')
+    .get(tourController.aliasTopTour, tourController.getAllTours);
+
+router.route('/stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+router
     .route('/')
     .get(tourController.getAllTours)
     .post(tourController.createTour);
