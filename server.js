db086592c2bf442ad8f3a55d056427d998cf3eba
Amrit Pant <info.amritpant@gmail.com>
2022-07-21 10:19:37 +0545

    Added two global error handlers ('unhandledRejection' , 'uncaughtException')

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('unhandledRejection', err => {
+    console.log(err.message);
+    console.log('UNAHNDLER REJECTION! Shutting down...');
+    process.exit(1);
+});
+
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+
+    process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
