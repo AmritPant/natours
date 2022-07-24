39e3e38f758d29a3ccfc407c9141966e2270ee09
Amrit Pant <info.amritpant@gmail.com>
2022-07-24 13:08:32 +0545

    Added Routes for Updating Password and Deleting User him/her self

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,4 +15,13 @@ userRouter.post('/login', authController.login);
 userRouter.post('/forgotPassword', authController.forgetPassword);
 userRouter.patch('/resetPassword/:token', authController.resetPassword);
 
+userRouter.patch(
+    '/updatePassword',
+    authController.protect,
+    authController.updatePassword
+);
+
+userRouter.patch('/updateMe', authController.protect, userController.updateMe);
+userRouter.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 module.exports = userRouter;
