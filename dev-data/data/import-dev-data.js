5ed6917f1b4a1c68b5579acedbfffe4d7ccf91f2
Amrit Pant <info.amritpant@gmail.com>
2022-07-26 09:36:45 +0545

    Configured Script such that It can add Tour,reviews,Users at one time

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: '../../config.env' });
 
@@ -14,11 +16,15 @@ mongoose.connect(DB).then(() => console.log(`DB Connection Sucessfull`));
 
 // Reading Tours from FILE
 const tours = JSON.parse(fs.readFileSync('./tours.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync('./reviews.json', 'utf-8'));
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log('Data sucessfully Loaded');
     } catch (err) {
         console.log(err);
@@ -30,7 +36,10 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany();
-        console.log('Data Successfully Deleted');
+        await User.deleteMany();
+        await Review.deleteMany();
+
+        await console.log('Data Successfully Deleted');
     } catch (err) {
         console.log(err);
     }
